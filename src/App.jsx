97c023b828e77fefd0785c5ce201355b7616a7f4
sebastian <sebastian.venegas172@gmail.com>
2024-06-24 14:01:06 -0400

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -2,61 +2,94 @@
 import './App.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {Card,Row,Col, Container} from 'react-bootstrap'
+import { Card, Row, Col, Container } from 'react-bootstrap'
 import { useState } from 'react';
 function App() {
- const [students,setStudents]=useState([]);
- const [name,setName]=useState('');
- const [age,setAge]=useState('');
+  const [students, setStudents] = useState([]);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [editIndex,setEditIndex]=useState(null);
 
- const handleSubmit=(event)=>{
-   event.preventDefault();
-   setStudents([...students,{name,age}])
-  // console.log(students)
-   setName('')
-   setAge('')
- }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(editIndex)
+
+    if(editIndex !== null){
+       const newStudents=[...students];
+       newStudents[editIndex]={name,age}
+      setStudents(newStudents)
+      setEditIndex(null)
+    }else{
+      setStudents([...students,{name,age}])
+    }
+    setName('')
+    setAge('')
+  }
+
+  const handleDelete=(index)=>{
+    const newStudents =[...students];
+    newStudents.splice(index,1);
+    setStudents(newStudents);
+
+  }
+
+  const handleEdit=(index)=>{
+    setName(students[index].name);
+    setAge(students[index].age);
+    setEditIndex(index);
+
+  }
 
   return (
     <>
-  <Container>
+      <Container>
 
-  <Row>
-    <Col>
-     
-     <Form onSubmit={handleSubmit}> 
-      <Form.Group className="mb-3">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese Nombre" value={name} onChange={(e)=>setName(e.target.value)}  />
-      </Form.Group>
-      <Form.Group className="mb-3">
-          <Form.Label>Edad</Form.Label>
-          <Form.Control type="number" placeholder="Ingrese Edad" value={age} onChange={(e)=>setAge(e.target.value)}/>
-      </Form.Group>
-        
-        <Button type="submit">Agregar Estudiante</Button>
+        <Row>
+          <Col>
 
-      </Form>
-    </Col>
-  </Row>
-   <Row>
-    {
-      students.map((student,index)=>(
-      <Col sm={6} key={index}>
-            <Card style={{ width:'18rem',marginTop:'20px'}}>
-          
-                <Card.Body>
-                  <Card.Title>Datos Estudiante</Card.Title>
-                  <Card.Text>Nombre:{student.name}</Card.Text>
-                  <Card.Text>Edad:{student.age}</Card.Text>
-                </Card.Body>
-            </Card>
-      </Col>
-      ))
-    }
-   </Row>
-       
-  </Container>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" placeholder="Ingrese Nombre" value={name} onChange={(e) => setName(e.target.value)} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Edad</Form.Label>
+                <Form.Control type="number" placeholder="Ingrese Edad" value={age} onChange={(e) => setAge(e.target.value)} />
+              </Form.Group>
+
+              <Button type="submit">
+                {
+                  editIndex!==null?'actualizar estudiantes':'Agregar Estudiantes'
+                }
+              </Button>
+
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          {
+            students.map((student, index) => (
+              <Col sm={6} key={index}>
+                <Card style={{ width: '18rem', marginTop: '20px' }}>
+
+                  <Card.Body>
+                    <Card.Title>Datos Estudiante</Card.Title>
+                    <Card.Text>Nombre:{student.name}</Card.Text>
+                    <Card.Text>Edad:{student.age}</Card.Text>
+                    <Button variant="danger" onClick={() => handleDelete(index)}>
+                      Eliminar
+                    </Button>
+                  <Button variant="warning" onClick={() => handleEdit(index)} style={{ marginLeft: '10px' }}>
+                      Editar
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+
+      </Container>
     </>
   )
 }
